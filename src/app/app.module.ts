5f039ee2ed7ef2cf468f00a067f09ce0be463482
naren0706 <narenthrakishhore@gmail.com>
2024-02-12 17:24:01 +0530

    [Narenthra Kishore] Profile animations

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,16 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { DashboardComponent } from './dashboard/dashboard.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgxParallaxModule } from '@yoozly/ngx-parallax';
+import { ContentsComponent } from './contents/contents.component';
+import { HomeComponent } from './sections/home/home.component';
+import { FormsComponent } from './sections/forms/forms.component';
+import { OutputComponent } from './sections/output/output.component';
+import { CreditsComponent } from './sections/credits/credits.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    DashboardComponent,
+    ContentsComponent,
+    HomeComponent,
+    FormsComponent,
+    OutputComponent,
+    CreditsComponent,
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    NgxParallaxModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/dashboard/dashboard.component.ts
@@ -0,0 +1,24 @@
+import {
+  trigger,
+  state,
+  style,
+  transition,
+  animate,
+} from '@angular/animations';
+import {
+  AfterViewInit,
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+} from '@angular/core';
+
+@Component({
+  selector: 'app-dashboard',
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.scss'],
+  
+})
+export class DashboardComponent {
+  
+}
